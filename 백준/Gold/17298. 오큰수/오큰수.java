fda3cae8ea2928ecb53bc328770d2273ccac6168
Hayoung Moon <124586544+gkdudans@users.noreply.github.com>
2025-03-17 12:01:46 +0900

    Update 오큰수.java

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -13,19 +13,19 @@ public class Main {
             A[i] = sc.nextInt();
         }
         
-        Arrays.fill(B, -1); // 모든 값을 -1로 초기화 (오큰수가 없는 경우)
+        Arrays.fill(B, -1);
         Stack<Integer> stack = new Stack<>();
+        // stack에는 index를 저장 
         
-        // 모든 원소를 처리 (1부터 n까지)
         for (int i = 1; i <= n; i++) {
-            // 스택이 비어있지 않고, 스택 맨 위 인덱스의 원소가 현재 원소보다 작으면
+            // 오큰수인 경우 
             while (!stack.isEmpty() && A[stack.peek()] < A[i]) {
-                // 오큰수 설정
                 B[stack.pop()] = A[i];
             }
             stack.push(i);
         }
         
+        // 출력 
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= n; i++) {
             sb.append(B[i]).append(" ");
