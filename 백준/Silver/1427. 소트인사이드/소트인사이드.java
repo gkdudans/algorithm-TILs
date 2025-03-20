55436c5e0cb18ad687f6cc638d53109e563f29f5
Hayoung Moon <124586544+gkdudans@users.noreply.github.com>
2025-03-20 12:50:59 +0900

    [Silver V] Title: 소트인사이드, Time: 164 ms, Memory: 17604 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,23 +1,36 @@
-//4. 소트인사이드
-import java.util.Scanner;
-import java.util.Arrays;
+import java.util.*;
 
-public class Main
-{
-	public static void main(String[] args) {
-	    Scanner sc = new Scanner(System.in);
-		String str = sc.next();
-		String[] num = str.split(""); //한 자리씩 나누기
+public class Main {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+        
+        String str = sc.next();
+		String[] num = str.split("");
 		
-		int[] arr = new int[num.length];
+		int[] A = new int[num.length];
+		
 		for(int i=0; i<num.length; i++){
-		    arr[i] = Integer.parseInt(num[i]);
+		    A[i] = Integer.parseInt(num[i]);
 		}
-		Arrays.sort(arr); 
-
-		for (int i = arr.length-1; i>=0; i--) {
-			System.out.print(arr[i]);
-		} //정렬한 뒤 내림차순으로 출력 
 		
-	}
+		for(int i=0; i<num.length; i++){
+		    int max = A[i];
+		    int maxIndex = i;
+		    for(int j=i; j<num.length; j++){
+		        if (max < A[j]){
+		            max = A[j];
+		            maxIndex = j;
+		        }
+		    }
+		    int tmp = A[i];
+		    A[i] = max;
+		    A[maxIndex] = tmp;
+
+		}
+		
+		for(int i=0; i<num.length; i++){
+		    System.out.print(A[i]);
+		}
+	
+    }
 }
