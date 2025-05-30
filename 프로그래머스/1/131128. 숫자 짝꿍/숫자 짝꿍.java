ee9123ce038265370845188a6037e1b4ed67392e
Hayoung Moon <124586544+gkdudans@users.noreply.github.com>
2025-05-30 23:55:05 +0900

    [level 1] Title: 숫자 짝꿍, Time: 63.67 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -4,46 +4,38 @@ import java.util.*;
 class Solution {
     static int[] arrX;
     static int[] arrY;
-    static int[] result;
+    
+    public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String X = br.readLine();
+        for(int i=0; i<X.length(); i++) {
+            arrX[i] = X.charAt(i);
+        }
+        
+        String Y = br.readLine();
+        for(int i=0; i<Y.length(); i++) {
+            arrY[i] = Y.charAt(i);
+        }
+        
+        Arrays.sort(arrX);
+        Arrays.sort(arrY);
+        
+        for(int i=0; i<arrX.length; i++) {
+            System.out.println(arrX[i]);
+        }
+        
+        
+        
+        
+        
+        
+	} 
+    
+    
 
     public String solution(String X, String Y) {
-        StringBuilder sb = new StringBuilder();
-        arrX = new int[10];
-        arrY = new int[10]; 
-        result = new int[10];
-        
-        for(int i=0; i<X.length(); i++) {
-            int c = X.charAt(i) - '0';
-            arrX[c]++;
-        }
-        
-        for(int i=0; i<Y.length(); i++) {
-            int c = Y.charAt(i) - '0';
-            arrY[c]++;
-        }
-        
-        for(int i=9; i>0; i--) {
-            result[i] = Math.min(arrX[i], arrY[i]);
-            for(int j=0; j<result[i]; j++) {
-                sb.append(i);
-            }
-        }
-        
-        if(sb.length() != 0) {
-            result[0] = Math.min(arrX[0], arrY[0]);
-            for(int j=0; j<result[0]; j++) {
-                sb.append(0);
-            }
-        } else {
-            result[0] = Math.min(arrX[0], arrY[0]);
-            if(result[0] > 0) sb.append(0);
-        }
-    
-        if (sb.length() == 0) {
-            return "-1";
-        }
-        
-        String answer = sb.toString();
+        String answer = "";
         return answer;
     }
 }
