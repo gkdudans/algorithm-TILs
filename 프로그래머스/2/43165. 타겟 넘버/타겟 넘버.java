7e26a9b43b97271c3195c55ae812a4af07a17107
Hayoung Moon <124586544+gkdudans@users.noreply.github.com>
2025-06-02 22:50:55 +0900

    [level 2] Title: 타겟 넘버, Time: 10.25 ms, Memory: 91.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,22 @@
+public class Solution {
+    private int answer = 0;
+
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, target, 0, 0);
+        return answer;
+    }
+
+    private void dfs(int[] numbers, int target, int index, int sum) {
+        if(index == numbers.length) {
+            if(sum == target) answer++;
+            return;
+            
+        } else if (index < numbers.length) {
+            dfs(numbers, target, index+1, sum + numbers[index]); 
+            dfs(numbers, target, index+1, sum - numbers[index]); 
+        } else {
+            return;
+        }
+    }
+}
+ 
