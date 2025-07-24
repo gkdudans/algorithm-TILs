bf40eb7aaac422c96f8c486c2579914a4376d559
Hayoung Moon <124586544+gkdudans@users.noreply.github.com>
2025-07-24 22:32:41 +0900

    [level 2] Title: 최솟값 만들기, Time: 6.19 ms, Memory: 54.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        
+        int answer = 0;
+        
+        PriorityQueue<Integer> AQueue = new PriorityQueue<>();
+        PriorityQueue<Integer> BQueue = new PriorityQueue<>(Collections.reverseOrder()); 
+
+        for(int i=0; i<A.length; i++) {
+            AQueue.add(A[i]);
+            BQueue.add(B[i]);
+        }
+        
+        for(int i=0; i<A.length; i++) {
+            answer += AQueue.poll() * BQueue.poll();
+        }
+        
+        
+        return answer;
+    }
+}
